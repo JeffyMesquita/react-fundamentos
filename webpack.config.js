dce8849f88a485be04df08459c02e886f0663552
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2025-01-24 12:51:30 -0300

    chore: Gerando o Bundle com o WebPack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+/*
+ * Why i should use path.resolve(__dirname, 'src/index.js') instead of 'src/index.js'?
+ * Because the path.resolve(__dirname, 'src/index.js') will return the absolute path of the file src/index.js
+ * and this is important because the webpack will use this path to create the bundle file.
+ * also, the __dirname is a global variable that will return the directory name of the current module.
+ * also too, runs to different operating systems, the path.resolve(__dirname, 'src/index.js') will return the correct path independently of the operating system.
+ */
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'transpiled', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle[hash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
