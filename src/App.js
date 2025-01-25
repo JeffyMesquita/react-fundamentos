7c0374724fbf0043ad72d9225766bd52ffc33bdc
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2025-01-25 19:57:28 -0300

    chore: React fragment

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 function App() {
   return (
-    <h1 id="title">
-      <span>Hello, world!</span>
-    </h1>
+    <>
+      <h1 id="title">
+        <span>Hello, world!</span>
+      </h1>
+
+      <h2>
+        <span>Good to see you!</span>
+      </h2>
+    </>
   );
 }
 
