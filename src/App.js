5905691549318e9c3a7ae7904f75963ffa69645d
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2025-02-02 16:59:07 -0300

    chore: Definindo tipos para as props

--- src/App.js
+++ src/App.js
@@ -6,22 +6,25 @@ import Header from './Header';
 function App() {
   return (
     <>
-      <Header title="JStack's Blog">
+      <Header>
         <h2>Posts da semana</h2>
       </Header>
 
       <hr />
 
       <Post
+        likes={20}
         post={{ title: 'Título da notícia', subtitle: 'subtitulo da notícia' }}
       />
       <Post
+        likes={30}
         post={{
           title: 'Outro título da notícia',
           subtitle: 'Outro subtitulo da notícia',
         }}
       />
       <Post
+        likes={40}
         post={{
           title: 'Mais um título da notícia',
           subtitle: 'Mais um subtitulo da notícia',
--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function Post(props) {
   return (
@@ -9,8 +10,18 @@ export default function Post(props) {
         <small>
           {props.post.subtitle ? props.post.subtitle : 'Sem subtítulo'}
         </small>
+        <br />
+        Média: {props.likes / 2}
       </article>
       <br />
     </>
   );
 }
+
+Post.propTypes = {
+  likes: PropTypes.number.isRequired,
+  post: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+  }).isRequired,
+};
