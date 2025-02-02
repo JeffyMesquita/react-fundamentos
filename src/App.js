62ec41860be55b6f8b6d735e51dbdc92e9073edb
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2025-02-02 16:45:22 -0300

    chore: Add Header component and update App to use it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Post from './Post';
+import Header from './Header';
 
 function App() {
   return (
     <>
-      <h1>JStacks's Blog</h1>
+      <Header title="JStack's Blog">
+        <h2>Posts da semana</h2>
+      </Header>
 
       <hr />
 
